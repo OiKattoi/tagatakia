ccd0b412c62555529438199ac1cd4545b2b8f6ec
ericosg <eric@loadx.org>
2022-11-06 13:29:51 +0200

    added firebase npm

--- src/index.js
+++ src/index.js
@@ -7,6 +7,28 @@ import webpackLogo from '@/images/webpack-logo.svg'
 // Test import of styles
 import '@/styles/index.scss'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Aj0XpkOjrghlFtQiknVzGUEAR85DJsE",
+  authDomain: "tagatakia-1b325.firebaseapp.com",
+  projectId: "tagatakia-1b325",
+  storageBucket: "tagatakia-1b325.appspot.com",
+  messagingSenderId: "485293927747",
+  appId: "1:485293927747:web:215ae521b883cb251753f6",
+  measurementId: "G-XHCQ7NK04C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Appending to the DOM
 const logo = document.createElement('img')
 logo.src = webpackLogo
@@ -22,5 +44,5 @@ imageBackground.classList.add('image')
 const imagePublic = document.createElement('img')
 imagePublic.src = '/assets/example.png'
 
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+const root = document.querySelector('#root')
+root.append(logo, heading, imageBackground, imagePublic)
